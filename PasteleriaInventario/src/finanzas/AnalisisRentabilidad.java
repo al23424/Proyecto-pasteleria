48583b17d11e96f915ac0fb6d2a0ff564269fafe
FaridALH19 <faridlupu29@gmail.com>
2025-08-30 21:39:24 -0500

    Update AnalisisRentabilidad.java

--- PasteleriaInventario/src/finanzas/AnalisisRentabilidad.java
+++ PasteleriaInventario/src/finanzas/AnalisisRentabilidad.java
@@ -1,5 +1,17 @@
 package finanzas;
 
 public class AnalisisRentabilidad {
+  public static double calcularGanancia(double ingresos, double gastos) {
+        return ingresos - gastos;
+    }
 
+    public static double calcularMargen(double ingresos, double ganancias) {
+        if (ingresos == 0) return 0;
+        return (ganancias / ingresos) * 100;
+    }
+
+    public static double calcularRentabilidad(double inversion, double ganancia) {
+        if (inversion == 0) return 0;
+        return (ganancia / inversion) * 100;
+    }
 }
