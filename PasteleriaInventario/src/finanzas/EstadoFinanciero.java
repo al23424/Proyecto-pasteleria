e1894f767e67672c18c3f240dd12666e94045497
FaridALH19 <faridlupu29@gmail.com>
2025-08-30 21:43:33 -0500

    Update EstadoFinanciero.java

--- PasteleriaInventario/src/finanzas/EstadoFinanciero.java
+++ PasteleriaInventario/src/finanzas/EstadoFinanciero.java
@@ -1,5 +1,16 @@
 package finanzas;
+import java.util.List;
 
 public class EstadoFinanciero {
+  public static double promedioGanancias(List<EstadoFinanciero> estados) {
+        return estados.stream().mapToDouble(EstadoFinanciero::getGanancias).average().orElse(0);
+    }
 
+    public static double promedioMargen(List<EstadoFinanciero> estados) {
+        return estados.stream().mapToDouble(EstadoFinanciero::getMargen).average().orElse(0);
+    }
+
+    public static EstadoFinanciero mejorPeriodo(List<EstadoFinanciero> estados) {
+        return estados.stream().max((a, b) -> Double.compare(a.getGanancias(), b.getGanancias())).orElse(null);
+    }
 }
