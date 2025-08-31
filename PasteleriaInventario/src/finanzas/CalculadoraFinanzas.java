fe6d2bbad0c0879c33a95c62284bddecd35b2cde
FaridALH19 <faridlupu29@gmail.com>
2025-08-30 21:41:18 -0500

    Update CalculadoraFinanzas.java

--- PasteleriaInventario/src/finanzas/CalculadoraFinanzas.java
+++ PasteleriaInventario/src/finanzas/CalculadoraFinanzas.java
@@ -1,5 +1,25 @@
 package finanzas;
 
 public class CalculadoraFinanzas {
+ private double ingresos;
+    private double gastos;
+    private double inversion;
 
+    public EstadoFinanciero(double ingresos, double gastos, double inversion) {
+        this.ingresos = ingresos;
+        this.gastos = gastos;
+        this.inversion = inversion;
+    }
+
+    public double getGanancias() {
+        return CalculadoraFinanzas.calcularGanancia(ingresos, gastos);
+    }
+
+    public double getMargen() {
+        return CalculadoraFinanzas.calcularMargen(ingresos, getGanancias());
+    }
+
+    public double getRentabilidad() {
+        return CalculadoraFinanzas.calcularRentabilidad(inversion, getGanancias());
+    }
 }
