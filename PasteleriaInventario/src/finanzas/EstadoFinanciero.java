19d6174c6aaa683eee144e4fffe5a8510e0d019e
FaridALH19 <faridlupu29@gmail.com>
2025-08-31 20:41:54 -0500

    Update EstadoFinanciero.java

--- PasteleriaInventario/src/finanzas/EstadoFinanciero.java
+++ PasteleriaInventario/src/finanzas/EstadoFinanciero.java
@@ -1,30 +1,28 @@
 package finanzas;
 
 public class EstadoFinanciero {
-    private double ingresos;
+   private double ingresos;
     private double gastos;
-    private double inversion;
-	public CalculadoraFinanzas(double ingresos, double gastos, double inversion) {
-		this.ingresos = ingresos;
-		this.gastos = gastos;
-		this.inversion = inversion;
-	}
-	public double getIngresos() {
-		return ingresos;
-	}
-	public void setIngresos(double ingresos) {
-		this.ingresos = ingresos;
-	}
-	public double getGastos() {
-		return gastos;
-	}
-	public void setGastos(double gastos) {
-		this.gastos = gastos;
-	}
-	public double getInversion() {
-		return inversion;
-	}
-	public void setInversion(double inversion) {
-		this.inversion = inversion;
-	}
+
+    public EstadoFinanciero(double ingresos, double gastos) {
+        this.ingresos = ingresos;
+        this.gastos = gastos;
+    }
+
+    // Getters y Setters
+    public double getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(double ingresos) {
+        this.ingresos = ingresos;
+    }
+
+    public double getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(double gastos) {
+        this.gastos = gastos;
+    }
 }
