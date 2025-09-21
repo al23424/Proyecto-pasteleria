dbdcfaf529660c9d9863dbd6e7a92338147a633b
sebas-ggg <sebastianjesusalberto@gmail.com>
2025-09-20 22:50:53 -0500

    noveno avance.java

--- PasteleriaInventario/src/modelo/Ingrediente.java
+++ PasteleriaInventario/src/modelo/Ingrediente.java
@@ -1,5 +1,31 @@
 package modelo;
 
 public class Ingrediente {
-
+    private int idIngrediente;
+    private String nombre;
+    private String unidadMedida; // kg, gramos, litros, etc.
+    private double cantidadStock;
+    private double stockMinimo;
+    private double precioUnitario;
+    private String proveedor;
+    
+    // Constructores
+    public Ingrediente() {}
+    
+    public Ingrediente(int idIngrediente, String nombre, String unidadMedida, 
+                      double cantidadStock, double stockMinimo, double precioUnitario) {
+        this.idIngrediente = idIngrediente;
+        this.nombre = nombre;
+        this.unidadMedida = unidadMedida;
+        this.cantidadStock = cantidadStock;
+        this.stockMinimo = stockMinimo;
+        this.precioUnitario = precioUnitario;
+    }
+    
+    // Método para verificar si necesita restock
+    public boolean necesitaRestock() {
+        return cantidadStock <= stockMinimo;
+    }
+    
+    // Getters y Setters completos
 }
