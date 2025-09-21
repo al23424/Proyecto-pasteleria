af12c9ba40e2374ca6cce87706633e81565530a8
sebas-ggg <sebastianjesusalberto@gmail.com>
2025-09-20 22:50:07 -0500

    octavo avance

--- PasteleriaInventario/src/modelo/Gasto.java
+++ PasteleriaInventario/src/modelo/Gasto.java
@@ -1,5 +1,26 @@
 package modelo;
 
-public class Gasto {
+import java.util.Date;
 
+public class Gasto {
+    private int idGasto;
+    private String descripcion;
+    private double monto;
+    private Date fecha;
+    private String categoria; // Ingredientes, Servicios, Equipos, etc.
+    private String proveedor;
+    
+    // Constructores
+    public Gasto() {}
+    
+    public Gasto(int idGasto, String descripcion, double monto, Date fecha, String categoria, String proveedor) {
+        this.idGasto = idGasto;
+        this.descripcion = descripcion;
+        this.monto = monto;
+        this.fecha = fecha;
+        this.categoria = categoria;
+        this.proveedor = proveedor;
+    }
+    
+    // Getters y Setters completos
 }
