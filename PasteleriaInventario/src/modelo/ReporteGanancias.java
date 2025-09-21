7bd5545a18d7221c24105f69044ae3e3c5eb40ef
sebas-ggg <sebastianjesusalberto@gmail.com>
2025-09-20 22:51:55 -0500

    undecimo avance.java

--- PasteleriaInventario/src/modelo/ReporteGanancias.java
+++ PasteleriaInventario/src/modelo/ReporteGanancias.java
@@ -1,5 +1,33 @@
 package modelo;
 
-public class ReporteGanancias {
+import java.util.Date;
 
+public class ReporteGanancias {
+    private int idReporte;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private double totalVentas;
+    private double totalGastos;
+    private double gananciasBrutas;
+    private double gananciasNetas;
+    
+    // Constructor
+    public ReporteGanancias(Date fechaInicio, Date fechaFin, double totalVentas, double totalGastos) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.totalVentas = totalVentas;
+        this.totalGastos = totalGastos;
+        this.gananciasBrutas = totalVentas;
+        this.gananciasNetas = totalVentas - totalGastos;
+    }
+    
+    // Método para calcular margen de ganancia
+    public double calcularMargenGanancia() {
+        if (totalVentas > 0) {
+            return (gananciasNetas / totalVentas) * 100;
+        }
+        return 0;
+    }
+    
+    // Getters y Setters
 }
