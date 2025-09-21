258475810148680b4bf990d0cc4f78e2bca7f44c
sebas-ggg <sebastianjesusalberto@gmail.com>
2025-09-20 22:49:24 -0500

    septimo avance

--- PasteleriaInventario/src/modelo/Cliente.java
+++ PasteleriaInventario/src/modelo/Cliente.java
@@ -1,5 +1,32 @@
 package modelo;
 
 public class Cliente {
-
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String direccion;
+    
+    // Constructor vacío
+    public Cliente() {}
+    
+    // Constructor con parámetros
+    public Cliente(int idCliente, String nombre, String apellido, String telefono, String email, String direccion) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+    
+    // Getters y Setters
+    public int getIdCliente() { return idCliente; }
+    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
+    
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    // ... resto de getters y setters
 }
