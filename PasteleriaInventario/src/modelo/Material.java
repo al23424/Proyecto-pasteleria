20d31e5a292901969eb30ca29a03de944f8e19a2
sebas-ggg <sebastianjesusalberto@gmail.com>
2025-09-20 22:51:31 -0500

    decimo avance

--- PasteleriaInventario/src/modelo/Material.java
+++ PasteleriaInventario/src/modelo/Material.java
@@ -1,5 +1,16 @@
 package modelo;
 
 public class Material {
-
+    private int idMaterial;
+    private String nombre;
+    private String tipo; // Envases, utensilios, decoración, etc.
+    private int cantidadStock;
+    private int stockMinimo;
+    private double precioUnitario;
+    
+    // Constructores y métodos similares a Ingrediente
+    
+    public boolean necesitaRestock() {
+        return cantidadStock <= stockMinimo;
+    }
 }
