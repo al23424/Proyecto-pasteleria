d6c2d68ec015d74f944baaa420bc762b44be8713
sebas-ggg <sebastianjesusalberto@gmail.com>
2025-09-20 22:52:15 -0500

    duodecimo avance

--- PasteleriaInventario/src/modelo/ReportePerdidas.java
+++ PasteleriaInventario/src/modelo/ReportePerdidas.java
@@ -1,5 +1,18 @@
 package modelo;
 
-public class ReportePerdidas {
+import java.util.Date;
 
+public class ReportePerdidas {
+    private int idReporte;
+    private Date fecha;
+    private String tipoProducto;
+    private int cantidadPerdida;
+    private String motivo; // Vencimiento, daño, robo, etc.
+    private double valorPerdida;
+    
+    // Constructores y métodos
+    
+    public double calcularImpactoEconomico() {
+        return valorPerdida;
+    }
 }
